16c0fc59b45fee9c6c1f6771194eac81ca80c00f
Andrea Bianco <andrea.bianco@truvis.ch>
2018-03-09 13:14:47 +0100

    [ADD] bug found

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -28,29 +28,34 @@ function init() {
           message: "Select output dir...",
           properties: ['openDirectory', 'createDirectory'],
         });*/
+        let arrayOfPromises = [];
         const sharpDir="/Users/bianco/here";
+        const inputImgPath = "/Users/bianco/Data/colorChecker/1/tiff/img_00001.tif";
         //console.log(inputImgPath);
-        sharp("/Users/bianco/Data/colorChecker/1/tiff/img_00001.tif")
-          .background({
-            r: 0,
-            g: 0,
-            b: 0
-          })
-          .extend({
-            top: 10,
-            bottom: 10,
-            left: 10,
-            right: 10
-          })
-          .jpeg()
-          .tile({
-            size: 512,
-            layout: 'google'
-          })
-          .toFile(sharpDir)
-          .then(info => {
-          console.log(info);
-          });
+        for (var i = 0; i < 10; i++) {
+          arrayOfPromises.push(sharp(inputImgPath)
+              .background({
+                r: 0,
+                g: 0,
+                b: 0
+              })
+              .extend({
+                top: 10,
+                bottom: 10,
+                left: 10,
+                right: 10
+              })
+              .jpeg()
+              .tile({
+                size: 512,
+                layout: 'google'
+              })
+              .toFile(sharpDir)
+          );
+
+        }
+        Promise.all(arrayOfPromises).then(info => {console.log(info);}).catch(info => {console.log(info);})
+
       }
     },
     template: `<div><div class=empty><p class="empty-title h5">Welcome to your new project!<p class=empty-subtitle>Get started now and take advantage of the great documentation at hand.<div class=empty-action><button @click="open('https://webpack.electron.build')"class="btn btn-primary">Documentation</button> <button @click="doTiles()"class="btn btn-primary">Sharp</button><button @click="open('https://electron.atom.io/docs/')"class="btn btn-primary">Electron</button><br><ul class=breadcrumb><li class=breadcrumb-item>electron-webpack v{{ versions.electronWebpack }}</li><li class=breadcrumb-item>electron v{{ versions.electron }}</li></ul></div><p class=footer>This intitial landing page can be easily removed from <code>src/renderer/index.js</code>.</p></div></div>`
